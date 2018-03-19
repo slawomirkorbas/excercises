9ba8ed713775c111b68df5e20a4bff3abdabceac
Slawomir Korbas <slawomirkorbas@gmail.com>
2018-03-19 22:27:22 +0100

    Cycle detection in linked list of nodes

---
+++ CycleDetection/src/main/java/cycle/detection/Node.java
@@ -0,0 +1,13 @@
+package cycle.detection;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: slawomir
+ * Date: 3/19/18
+ * Time: 10:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Node {
+    public int data;
+    public Node next;
+}
