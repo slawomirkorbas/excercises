1b393f36558ba71597a44ff80785e281ec486623
Slawomir Korbas <slawomir.korbas@sonalake.com>
2018-04-12 11:58:49 +0200

    initial commit

---
+++ NoughtsAndCrosses/src/main/java/game/controller/MainController.java
@@ -0,0 +1,26 @@
+package game.controller;
+
+import game.application.GameState;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MainController {
+
+//    @RequestMapping("/")
+//    public String index() {
+//        return "index";
+//    }
+
+    @RequestMapping( value = "/gameState/", produces = MediaType.APPLICATION_JSON_VALUE )
+    public @ResponseBody
+    GameState countryByCode(@RequestParam("matrix") String matrix) {
+
+        return new GameState();
+    }
+}
