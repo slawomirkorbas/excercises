4066e427319608adf9fee65143687320f644e579
Slawomir Korbas <slawomir.korbas@sonalake.com>
2018-04-20 09:10:48 +0200

    changes

--- NoughtsAndCrosses/src/main/webapp/gameLogic.js
+++ NoughtsAndCrosses/src/main/webapp/gameLogic.js
@@ -39,6 +39,7 @@ function evaluatePossibleGames(matrix, currentFigure, evaluation, movesCount ) {
     if( gameResult == COMPUTER_LOST ) {
         evaluation.games += 1;
         evaluation.loses++;
+        evaluation.movesTotal += movesCount;
         if( movesCount == 2 ) {
             // 2 means we are losing in the next opponents move - this should be avoided game solution so we give it -100
             evaluation.pts -= 100;
@@ -48,10 +49,15 @@ function evaluatePossibleGames(matrix, currentFigure, evaluation, movesCount ) {
         }
     }
     else if( gameResult == COMPUTER_WIN ) {
+        if( movesCount == 3 ) {
+            // 3 means computer is able to win in the next move - this should be recommended game solution so we give it +100
+            evaluation.movesTotal += 100;
+        }
+        else {
+            evaluation.pts += (1 + emptyFieldsLeft(matrix));
+        }
         evaluation.movesTotal += movesCount;
         evaluation.wins++;
-        evaluation.pts += (1 + emptyFieldsLeft(matrix));
-        evaluation.movesTotal += movesCount;
     }
     else if( matrixFull(matrix) ) {
         evaluation.draws++;
@@ -64,7 +70,7 @@ function evaluatePossibleGames(matrix, currentFigure, evaluation, movesCount ) {
             for( var c=0; c < matrix.length; c++ ) {
                 var matrixCopy = copyMatrix( matrix );
                 var moves = movesCount;
-                if( matrixCopy[r][c] == '' && isOpponentsTurn(currentFigure) && (countOccupiedFields(matrixCopy) < totalFields - 1) && deadField(matrixCopy,r,c) ) {
+                if( false ){// matrixCopy[r][c] == '' && isOpponentsTurn(currentFigure) && (countOccupiedFields(matrixCopy) < totalFields - 1) && deadField(matrixCopy,r,c) ) {
                     // don't evaluate games with not clever moves...
                     continue;
                 }
