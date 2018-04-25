af6aa31d974000e2f25232485a89b3972a0c68cf
slawomirkorbas <slawomirkorbas@gmail.com>
2018-04-25 15:13:50 +0200

    Update gameLogic.js

--- NoughtsAndCrosses/src/main/webapp/gameLogic.js
+++ NoughtsAndCrosses/src/main/webapp/gameLogic.js
@@ -43,15 +43,14 @@ function evaluatePossibleGames(matrix, currentFigure, evaluation, movesCount ) {
         if( movesCount == 2 ) {
             // 2 means we are losing in the next opponents move - this should be avoided game solution so we give it -100
             evaluation.pts -= 100;
-        }
-        else {
+        } else {
             evaluation.pts -= (1 + emptyFieldsLeft(matrix));
         }
     }
     else if( gameResult == COMPUTER_WIN ) {
-        if( movesCount == 3 ) {
-            // 3 means computer is able to win in the next move - this should be recommended game solution so we give it +100
-            evaluation.movesTotal += 100;
+        if( movesCount == 1 ) {
+           // test  
+            evaluation.pts += 100;
         }
         else {
             evaluation.pts += (1 + emptyFieldsLeft(matrix));
@@ -69,15 +68,13 @@ function evaluatePossibleGames(matrix, currentFigure, evaluation, movesCount ) {
         for( var r=0; r < matrix.length; r++ ) {
             for( var c=0; c < matrix.length; c++ ) {
                 var matrixCopy = copyMatrix( matrix );
-                var moves = movesCount;
-                if( false ){// matrixCopy[r][c] == '' && isOpponentsTurn(currentFigure) && (countOccupiedFields(matrixCopy) < totalFields - 1) && deadField(matrixCopy,r,c) ) {
+                if( matrixCopy[r][c] == '' && isOpponentsTurn(currentFigure) && (countOccupiedFields(matrixCopy) < totalFields - 1) && deadField(matrixCopy,r,c) ) {
                     // don't evaluate games with not clever moves...
                     continue;
                 }
                 else  if( matrixCopy[r][c] == '') {
                     matrixCopy[r][c] = currentFigure;
-                    moves += 1;
-                    evaluation = evaluatePossibleGames(matrixCopy, toggle(currentFigure), evaluation, moves );
+                    evaluation = evaluatePossibleGames(matrixCopy, toggle(currentFigure), evaluation, movesCount + 1 );
                 }
             }
         }
